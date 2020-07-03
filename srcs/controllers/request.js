5429b47d4eb49453164bacf1f20a8f2862420d3a
Joaquim Castro <joaquimcastro2909@gmail.com>
2020-07-02 23:31:17 -0300

    -Estruturação basica e request terminados

--- srcs/controllers/request.js
+++ srcs/controllers/request.js
@@ -1,22 +1,28 @@
 let apodToJson;
 
-var xhr = new XMLHttpRequest();
-
+const xhr = new XMLHttpRequest();
 xhr.open("GET", "https://api.nasa.gov/planetary/apod?api_key=ISIn2PcRcmIDY7CkSgMEzN5W0b8uxCa2c04sNh48");
-
 xhr.send();
 
-xhr.addEventListener("load", function () {
+xhr.onload = function () {
 
-  apodToJson = JSON.parse(xhr.responseText);
+  if (xhr.status == 200) {
+    apodToJson = JSON.parse(xhr.responseText);
+    let dateReoriented = [apodToJson.date.split("-")[2], apodToJson.date.split("-")[1], apodToJson.date.split("-")[0]];
 
-  apodData = {
-    apodTitle: apodToJson.title,
-    apodMedia: apodToJson.url,
-    apodMediaType: apodToJson.media_type,
-    apodExpl: apodToJson.explanation,
-    apodCopyright: apodToJson.copyright
-  };
+    apodData = {
+      apodTitle: apodToJson.title,
+      apodDate: dateReoriented.join("/"),
+      apodMedia: apodToJson.url,
+      apodMediaType: apodToJson.media_type,
+      apodExpl: apodToJson.explanation,
+      apodCopyright: apodToJson.copyright
+    };
 
-  mediaContainer();
-});
+    sobeTituloMedia();
+    sobeData();
+    sobeImagem();
+    sobeExpl();
+    sobeCopyright();
+  }
+};
--- srcs/views/index.js
+++ srcs/views/index.js
@@ -1,13 +1,23 @@
 let elementosPagina = {
-  linhaData: document.querySelector("#linha-data"),
+  mediaData: document.querySelector("#media-data"),
+  mediaTitle: document.querySelector("#media-title"),
   mediaContainer: document.querySelector("#media-container"),
-
+  mediaExpl: document.querySelector("#linha-expl"),
+  mediaCopyright: document.querySelector("#copyrigth")
 };
 
-let mediaContainer = function () {
-  elementosPagina.mediaContainer.href = this.apodData.apodMedia;
+function sobeTituloMedia() {
+  elementosPagina.mediaTitle.textContent = apodData.apodTitle;
+}
 
-  if (this.apodData.apodMediaType == "image") {
+function sobeData() {
+  elementosPagina.mediaData.textContent = apodData.apodDate;
+}
+
+function sobeImagem() {
+  elementosPagina.mediaContainer.href = apodData.apodMedia;
+
+  if (apodData.apodMediaType == "image") {
 
     let imgContainer = document.createElement("img");
 
@@ -15,9 +25,9 @@ let mediaContainer = function () {
 
     elementosPagina.mediaContainer.appendChild(imgContainer);
 
-    document.querySelector(".media").src = this.apodData.apodMedia;
+    document.querySelector(".media").src = apodData.apodMedia;
 
-  } else if (this.apodData.apodMediaType == "video") {
+  } else if (apodData.apodMediaType == "video") {
 
     let videoContainer = document.createElement("video");
 
@@ -25,7 +35,21 @@ let mediaContainer = function () {
 
     elementosPagina.mediaContainer.appendChild(videoContainer);
 
-    document.querySelector(".media").src = this.apodData.apodMedia;
+    document.querySelector(".media").src = apodData.apodMedia;
 
   }
-};
+}
+
+function sobeExpl() {
+  if (apodData.apodMedia == "https://api.nasa.gov/planetary/apod/static/default_apod_image.jpg") {
+    elementosPagina.mediaExpl.textContent = "This is a fallback image used in the case where there is a missing/corrupted asset on apod.nasa.gov.Image source: https://en.wikipedia.org/wiki/File:Black_Hole_in_the_universe.jpg";
+  } else {
+    elementosPagina.mediaExpl.textContent = apodData.apodExpl;
+  }
+}
+
+function sobeCopyright() {
+  if (apodData.apodCopyright != undefined) {
+    elementosPagina.mediaCopyright.textContent = apodData.apodCopyright;
+  }
+}
